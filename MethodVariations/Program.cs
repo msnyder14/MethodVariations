1281d427bea5fdcd4aadf31827b1391c5225fd1d
FSW <FSW@LE-K107-PO-NN37>
2025-01-16 13:36:32 -0500

    Add project files.

---
+++ MethodVariations/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace MethodVariations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            float numerator = 17f;
+            float denominator = 3f;
+            float result = numerator / denominator;
+            
+            Console.WriteLine("{numerator} / {denominator} = {result}");
+        }
+
+        void TryDividing(float numerator, float denominator)
+        {
+            float result = numerator / denominator;
+        }
+
+        float Divide(float numerator, float denominator)
+        {
+            return numerator / denominator;
+        }
+
+        void DivideByRef(float numerator, float denominator, ref float result)
+        {
+            result = numerator / denominator;
+        }
+
+        void DivideByRefSafe(float numerator, float denominator, ref float result)
+        {
+            result = denominator == 0f ? 0f : numerator / denominator;
+        }
+
+        bool DivideByRefSafeAndRobust(float numerator, float denominator, ref float result)
+        {
+            result = denominator == 0f ? 0f : numerator / denominator;
+            return denominator == 0f;
+        }
+
+        object DivideAnonymous(float numerator, float denominator)
+        {
+            return new {
+                ErrorCode = denominator == 0f,
+                Result = denominator == 0f ? 0f : numerator / denominator
+            };
+        }
+
+        Tuple<bool, float> DivideStrong(float numerator, float denominator)
+        {
+            return new Tuple<bool, float>(denominator == 0, denominator == 0f ? 0f : numerator / denominator);
+        }
+    }
+}
